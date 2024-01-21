d6355e00d81f4528b89700fef621db7ae172bd5b
see-quick <maros.orsak159@gmail.com>
2024-01-21 15:38:00 +0100

    PodChaos random leader killed
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- strimkkhaos.sh
+++ strimkkhaos.sh
@@ -1,82 +1,11 @@
 #!/bin/bash
 
-# Basic Chaos Testing Script for Strimzi using Chaos Mesh in OpenShift
+# Mandatory environment variables
+PROMETHEUS_URL="${PROMETHEUS_URL:-http://default-prometheus-url}" # Replace 'http://default-prometheus-url' with your default Prometheus URL
 
-# Function to display usage information
-usage() {
-    echo "Usage: $0 [options]"
-    echo "Options:"
-    echo "  -h, --help                 Show this help message"
-    echo "  --install                  Install Chaos Mesh"
-    echo "  --uninstall                Uninstall Chaos Mesh"
-    echo "  --pod-chaos                Apply a PodChaos experiment"
-    echo "  --network-chaos            Apply a NetworkChaos experiment"
-    echo "  --monitor-log              Monitor and log Chaos Mesh activities"
-    echo "  --cleanup                  Cleanup after experiment"
-    echo "  --release-name NAME        Specify the release name for Chaos Mesh (default: 'chaos-mesh')"
-    echo "  --namespace NS             Specify the namespace for Chaos Mesh (default: 'chaos-mesh')"
-    echo "  --version VER              Specify the version of Chaos Mesh (default: '2.6.2')"
-    echo "  --openshift                Indicate that the script is running in an OpenShift environment"
-    echo ""
-    echo "Example:"
-    echo "  $0 --install --release-name my-chaos --namespace my-namespace --version 2.6.2"
-}
-
-# Function to echo success messages in green with a [SUCCESS] prefix
-echo_success() {
-    echo -e "\033[0;32m[SUCCESS] $1\033[0m"
-}
-
-# Function to echo warning messages in yellow with a [WARNING] prefix
-echo_warning() {
-    echo -e "\033[0;33m[WARNING] $1\033[0m"
-}
-
-# Function to echo error messages in red with an [ERROR] prefix
-echo_error() {
-    echo -e "\033[0;31m[ERROR] $1\033[0m"
-}
-
-# Function to deploy Kafka producers
-deploy_kafka_producers() {
-    for i in {1..10}; do
-        kubectl apply -f <(sed "s/{{INDEX}}/$i/g" chaos-manifests/producer/producer.yaml) -n myproject
-    done
-    echo_success "Kafka producers deployed."
-}
-
-# Function to delete Kafka producers
-delete_kafka_producers() {
-    for i in {1..10}; do
-        kubectl delete job kafka-producer-client-$i -n myproject
-    done
-    echo_success "Kafka producers deleted."
-}
-
-# Function to query Prometheus from inside a Kafka pod using label
-query_prometheus_from_pod() {
-    local namespace=$1
-    local label=$2
-    local prometheus_url=$3
-    local query=$4
-
-    # Get the pod name dynamically based on label
-    local pod_name=$(get_pod_name_by_label "$namespace" "$label")
-    if [ -z "$pod_name" ]; then
-        echo_error "No pod found with label $label in namespace $namespace"
-        return 1
-    fi
-
-    # Executing the query inside the Kafka pod
-    kubectl exec "$pod_name" -n "$namespace" -- curl -G --data-urlencode "query=$query" "$prometheus_url/api/v1/query"
-}
-
-# Function to get pod name by label
-get_pod_name_by_label() {
-    local namespace=$1
-    local label=$2
-    kubectl get pods -n "$namespace" -l "$label" -o jsonpath='{.items[0].metadata.name}'
-}
+#####################################################################################################################
+################################# CHAOS MESH INSTALL/UNINSTALL  #####################################################
+#####################################################################################################################
 
 # Function to install Chaos Mesh using Helm and verify that all pods are running
 install_chaos_mesh() {
@@ -85,13 +14,19 @@ install_chaos_mesh() {
     local cm_version=$3
     local openshift_flag=$4
 
+    # Check if the namespace exists, create if not
+    if ! kubectl get namespace "$namespace" &> /dev/null; then
+        echo_warning "Namespace $namespace does not exist. Creating it..."
+        kubectl create namespace "$namespace"
+        echo_success "Namespace $namespace created."
+    fi
+
     echo "Installing Chaos Mesh version $cm_version in namespace $namespace with release name $release_name"
     # Installation command (using Helm)
     helm install $release_name chaos-mesh/chaos-mesh -n $namespace \
         --set chaosDaemon.runtime=crio \
         --set chaosDaemon.socketPath=/var/run/crio/crio.sock \
         --set chaosDaemon.env.RUST_BACKTRACE=full \
-        # this is for debugging IOChaos (i.e., TODA tool)
         --version $cm_version
 
     # Wait for all Chaos Mesh pods to be running
@@ -160,6 +95,185 @@ uninstall_chaos_mesh() {
     fi
 }
 
+#####################################################################################################################
+#####################################################################################################################
+#####################################################################################################################
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -h, --help                 Show this help message"
+    echo "  --install                  Install Chaos Mesh"
+    echo "  --uninstall                Uninstall Chaos Mesh"
+    echo "  --pod-chaos                Apply a PodChaos experiment"
+    echo "  --release-name NAME        Specify the release name for Chaos Mesh (default: 'chaos-mesh')"
+    echo "  --namespace NS             Specify the namespace for Chaos Mesh (default: 'chaos-mesh')"
+    echo "  --version VER              Specify the version of Chaos Mesh (default: '2.6.2')"
+    echo "  --openshift                Indicate that the script is running in an OpenShift environment"
+    echo ""
+    echo "Example:"
+    echo "  $0 --install --release-name my-chaos --namespace my-namespace --version 2.6.2"
+}
+
+# Function to echo success messages in green with a [SUCCESS] prefix
+echo_success() {
+    echo -e "\033[0;32m[SUCCESS] $1\033[0m"
+}
+
+# Function to echo warning messages in yellow with a [WARNING] prefix
+echo_warning() {
+    echo -e "\033[0;33m[WARNING] $1\033[0m"
+}
+
+# Function to echo error messages in red with an [ERROR] prefix
+echo_error() {
+    echo -e "\033[0;31m[ERROR] $1\033[0m"
+}
+
+#####################################################################################################################
+################################################ POD CHAOS  #########################################################
+#####################################################################################################################
+
+############################################### KILLING LEADER ######################################################
+
+# Function to apply PodChaos
+apply_podchaos() {
+    local template_yaml="./chaos-manifests/pod_chaos/kafka_random_pod_kill.yaml"
+    local experiment_name=$(generate_experiment_name)
+
+    # Check if the template YAML file exists
+    if [ ! -f "$template_yaml" ]; then
+        echo_error "PodChaos template YAML file does not exist at path: $template_yaml"
+        return 1
+    fi
+
+    sed -i '' "s/name: .*/name: $experiment_name/" "$template_yaml"
+
+    echo "Applying PodChaos from YAML manifest: $template_yaml"
+    kubectl apply -f "$template_yaml"
+
+    echo_success "PodChaos applied using manifest $template_yaml."
+}
+
+# Function to check if the PodChaos experiment has started
+check_podchaos_started() {
+    # Get the status of the PodChaos object
+    pod_chaos_status=$(oc get PodChaos kafka-random-pod-kill -n strimzi-kafka -o json)
+
+    # Check if AllInjected is True
+    all_injected=$(echo "$pod_chaos_status" | jq -r '.status.conditions[] | select(.type=="AllInjected") | .status')
+
+    # Check the desiredPhase
+    desired_phase=$(echo "$pod_chaos_status" | jq -r '.status.experiment.desiredPhase')
+
+    # Determine if the experiment has started
+    if [[ "$all_injected" == "True" && "$desired_phase" == "Run" ]]; then
+        echo_success "PodChaos experiment has started."
+    else
+        echo_warning "PodChaos experiment has not started or is completed."
+    fi
+}
+
+# Function to scrape Prometheus metrics at regular intervals
+scrape_metrics_during_chaos() {
+    local query_expr=$1
+    local interval=$2
+    local duration=$3
+    local end_time=$((SECONDS + duration))
+    local total=0
+    local count=0
+
+    while [ $SECONDS -lt $end_time ]; do
+        local result=$(curl -s -G --data-urlencode "query=$query_expr" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[0].value[1]')
+        total=$(echo "$total + $result" | bc)
+        count=$((count + 1))
+        sleep $interval
+    done
+
+    if [ $count -gt 0 ]; then
+        local average=$(echo "$total / $count" | bc -l)
+        local round_average_two_decimals=$(round $average)
+        echo "$round_average_two_decimals"
+    else
+        echo_error "No metrics scraped."
+        return 1
+    fi
+}
+
+# Function to create Prometheus query expression with customizable time range
+build_query_expr() {
+    local time_range=$1
+    local function_name="rate"
+
+    if [[ $time_range == "1m" ]]; then
+        function_name="irate" # Use irate for short time range because, it calculates the rate of increase using the last two points in the provided range
+    fi
+
+    echo "sum(${function_name}(kafka_server_brokertopicmetrics_messagesin_total{namespace=\"strimzi-kafka\",strimzi_io_cluster=\"anubis\",topic=~\".+\",topic!=\"\",kubernetes_pod_name=~\"anubis-.*\", clusterName=~\"worker-01\"}[${time_range}]))"
+}
+
+round() {
+  printf "%.2f" "$(echo "scale=2; $1/1" | bc -l)"
+}
+
+# Function to verify decrease in Kafka throughput
+verify_kafka_throughput() {
+    local kafka_query_expr=$(build_query_expr "1h")
+
+    # NORMAL AVERAGE compute based on 1h
+    local normal_average=$(curl -s -G --data-urlencode "query=$kafka_query_expr" "$PROMETHEUS_URL/api/v1/query" | jq -r '.data.result[0].value[1]')
+    echo "Normal average of messages in the past hour is $normal_average"
+
+    kafka_query_expr=$(build_query_expr "5m")
+
+    local scrape_interval=1
+    local chaos_duration=300 # Duration of chaos experiment in seconds
+    local chaos_average=$(scrape_metrics_during_chaos "$kafka_query_expr" $scrape_interval $chaos_duration)
+
+    normal_average=$(round $normal_average)
+
+    # Perform the comparison using bc
+    result=$(echo "${chaos_average} < ${normal_average}" | bc -l)
+
+    if [[ $result -eq 1 ]]; then
+        echo_success "Verified expected decrease in Kafka throughput after chaos experiment: chaos average msg/s is ${chaos_average} which is lower than normal average i.e., ${normal_average}"
+    else
+        echo_error "Kafka throughput did not decrease as expected: chaos average msg/s is ${chaos_average} which is greater than normal average i.e., ${normal_average}"
+    fi
+}
+
+# Function to generate the next experiment name
+generate_experiment_name() {
+    local base_name="kafka-kill-random-leader"
+
+    # Get the list of existing PodChaos objects with the base name
+    local existing_names=$(kubectl get PodChaos -n strimzi-kafka -o jsonpath="{.items[?(@.metadata.name startsWith ${base_name})].metadata.name}")
+
+    # Find the highest number used so far
+    local max_number=-1
+    for name in $existing_names; do
+        local number=$(echo "$name" | sed -e "s/^${base_name}-//")
+        if [[ "$number" =~ ^[0-9]+$ ]] && [ "$number" -gt "$max_number" ]; then
+            max_number=$number
+        fi
+    done
+
+    # Generate the next number
+    local next_number=$((max_number + 1))
+
+    # Construct the full experiment name
+    echo "${base_name}-${next_number}"
+}
+
+#####################################################################################################################
+#####################################################################################################################
+#####################################################################################################################
+
+
+#####################################################################################################################
+########################################### AUXILIARY METHODS #######################################################
+#####################################################################################################################
 
 # Function to check if Chaos Mesh is installed
 check_chaos_mesh() {
@@ -171,117 +285,24 @@ check_chaos_mesh() {
     fi
 }
 
-# Function to apply a chaos experiment
-apply_chaos_experiment() {
-    local experiment_file=$1
-    if [ -f "$experiment_file" ]; then
-        echo "Applying chaos experiment from $experiment_file"
-        kubectl apply -f "$experiment_file"
-    else
-        echo_error "Experiment file $experiment_file not found."
-        exit 1
-    fi
-}
-
-# Function to apply and monitor PodChaos experiment
-apply_and_monitor_podchaos() {
-    local experiment_file=$1
-    local target_pod_label=$2
-    local namespace=$3
-    local experiment_duration=$4 # in seconds
-    local experiment_name=$5
-
-    # Apply the chaos experiment
-    apply_chaos_experiment "$experiment_file"
-
-    # Start monitoring
-    monitor_and_log "$target_pod_label" "$namespace" "$experiment_duration" "$experiment_name"
-
-    # Use chaosctl for additional diagnostics (example)
-    echo "Using chaosctl for diagnostics"
-    chaosctl logs -n $namespace
-}
-
-# Function to monitor and log results along with verifying the Chaos Mesh experiment status
-monitor_and_log() {
-    local target_pod_label=$1
-    local namespace=$2
-    local experiment_duration=$3 # in seconds
-    local experiment_name=$4     # name of the chaos experiment
-
-    echo "Starting PodChaos experiment monitoring at $(date)"
-    local start_time=$(date +%s)
-    local current_time=$start_time
-    local end_time=$(($start_time + $experiment_duration))
-
-    while [ $current_time -le $end_time ]; do
-        echo "Checking pod status at $(date)..."
-        kubectl get pods -n $namespace -l $target_pod_label
-
-        echo "Verifying Chaos Mesh experiment status..."
-        local experiment_status=$(kubectl get podchaos $experiment_name -n $namespace -o json)
-
-         # Preprocess JSON output to escape control characters
-        local cleaned_status=$(echo $experiment_status | tr -d '\n' | tr -d '\r')
-
-        # Check if the JSON object is not null and has the expected structure
-        if [[ ! -z "$cleaned_status" && $(echo $cleaned_status | jq -r '.status.conditions') != "null" ]]; then
-            local all_injected=$(echo $cleaned_status | jq -r '.status.conditions[] | select(.type=="AllInjected") | .status')
-            local all_recovered=$(echo $cleaned_status | jq -r '.status.conditions[] | select(.type=="AllRecovered") | .status')
-            echo "Chaos Mesh Experiment Status: Injected: $all_injected, Recovered: $all_recovered"
-        else
-            echo_warning "Chaos Mesh Experiment status is not available or not ready."
-        fi
-
-        sleep 5
-        current_time=$(date +%s)
-    done
-
-    echo_success "PodChaos experiment duration completed at $(date). Verifying pod recovery..."
-    kubectl get pods -n $namespace -l $target_pod_label
-}
-
-
-# Function to cleanup Chaos Mesh experiment of a specific type
-cleanup_chaos_experiment() {
-    local experiment_type=$1
-    local experiment_name=$2
-    local namespace=$3
-
-    echo "Cleaning up $experiment_type experiment $experiment_name from namespace $namespace..."
-    kubectl delete $experiment_type $experiment_name -n $namespace
-
-    # Check if the experiment is successfully deleted
-    if kubectl get $experiment_type $experiment_name -n $namespace &> /dev/null; then
-        echo_error "Failed to delete $experiment_type experiment $experiment_name."
-        exit 1
-    else
-        echo_success "$experiment_type experiment $experiment_name successfully deleted."
-    fi
-}
-
 # Main function to parse arguments and execute commands
 main() {
     local install_flag=false
     local uninstall_flag=false
-    local apply_experiment_flag=false
-    local monitor_log_flag=false
-    local cleanup_flag=false
     local openshift_flag=false
     local pod_chaos_flag=false
-    local network_chaos_flag=false
 
-   # Default values for variables
-   local release_name="chaos-mesh"
-   local namespace="chaos-mesh"
-   local cm_version="2.6.2"
+    # Default values for variables
+    local release_name="chaos-mesh"
+    local namespace="chaos-mesh"
+    local cm_version="2.6.2"
 
-   if [[ $# -eq 0 ]]; then
-       usage
-       exit 1
-   fi
+    if [[ $# -eq 0 ]]; then
+        usage
+        exit 1
+    fi
 
-   while [[ $# -gt 0 ]]; do
+    while [[ $# -gt 0 ]]; do
        key="$1"
        case "$key" in
            -h|--help)
@@ -298,14 +319,6 @@ main() {
                ;;
            --pod-chaos)
                pod_chaos_flag=true
-               shift
-               ;;
-           --network-chaos)
-               network_chaos_flag=true
-               shift
-               ;;
-           --cleanup)
-               cleanup_flag=true
                shift
                ;;
            --release-name)
@@ -332,7 +345,7 @@ main() {
                shift
                ;;
            *)
-               echo "Unknown option $key"
+               echo_error "Unknown option $key"
                usage
                exit 1
                ;;
@@ -348,40 +361,12 @@ main() {
         uninstall_chaos_mesh "$release_name" "$namespace"
     fi
 
-    # For PodChaos experiment (modify as per your needs)
     if $pod_chaos_flag; then
-        apply_and_monitor_podchaos "./chaos-manifests/pod_chaos/pod-failure.yaml" "strimzi.io/kind=cluster-operator" "myproject" 300 "strimzi-operator-failure-example"
-    fi
+        apply_podchaos
 
-    # Example usage for cleanup
-    if $cleanup_flag; then
-        # TODO: make this as paramter now only podchaos
-        cleanup_chaos_experiment "podchaos" "strimzi-operator-failure-example" "myproject"
-        cleanup_chaos_experiment "networkchaos" "kafka-network-latency-all" "myproject"
-    fi
+        check_podchaos_started
 
-    if $network_chaos_flag; then
-        deploy_kafka_producers
-
-        namespace="myproject"
-        label="strimzi.io/kind=cluster-operator"  # Replace with the actual label of your Kafka pod
-        prometheus_url="prometheus-operated:9090"
-        metric_query="sum(irate(kafka_server_brokertopicmetrics_messagesin_total{namespace=\"$kubernetes_namespace\",strimzi_io_cluster=\"$strimzi_cluster_name\",topic=~\"$kafka_topic\",topic!=\"\",kubernetes_pod_name=~\"$strimzi_cluster_name-$kafka_broker\"}[1m]))"
-
-        echo "Querying Prometheus before chaos..."
-        query_result=$(query_prometheus_from_pod "$namespace" "$label" "$prometheus_url" "$metric_query")
-        echo "Metrics before chaos: $query_result"
-
-        apply_chaos_experiment "./chaos-manifests/network_chaos/network_latency_producers.yaml"
-
-        echo "Monitoring metrics for 1 minutes..."
-        sleep 60
-
-        echo "Querying Prometheus after chaos..."
-        query_result=$(query_prometheus_from_pod "$namespace" "$label" "$prometheus_url" "$metric_query")
-        echo "Metrics after chaos: $query_result"
-
-        delete_kafka_producers
+        verify_kafka_throughput
     fi
 }
 
